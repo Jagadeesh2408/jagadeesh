3f4ec32edc808d1abce5dabbe1990910a640df40
jagadeesh2408 <puligundlajagadeesh@gmail.com>
2022-12-02 00:01:05 +0530

    login test update

--- Maven_01_12/src/test/java/com/sdet/LogIN_Test.java
+++ Maven_01_12/src/test/java/com/sdet/LogIN_Test.java
@@ -1,5 +1,23 @@
 package com.sdet;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class LogIN_Test {
+	
+	public void logIntest() {
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver=new EdgeDriver();
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.xpath("//div[text()='Login ']")).click();
+		
+		
+		Assert.assertEquals(driver.getTitle(), "actiTIME - Enter Time-Track");
+	}
 
 }
