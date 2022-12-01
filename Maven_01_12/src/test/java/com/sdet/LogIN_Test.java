b9072d59d60d34ac274cda7beae2c27218baf272
jagadeesh2408 <puligundlajagadeesh@gmail.com>
2022-12-02 00:37:08 +0530

    After exeguting the login test

--- Maven_01_12/src/test/java/com/sdet/LogIN_Test.java
+++ Maven_01_12/src/test/java/com/sdet/LogIN_Test.java
@@ -4,17 +4,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LogIN_Test {
-	
-	public void logIntest() {
+	@Test
+	public void logIntest() throws InterruptedException {
 		WebDriverManager.edgedriver().setup();
 		WebDriver driver=new EdgeDriver();
+		Thread.sleep(2000);
+		driver.get("https://demo.actitime.com/");
+		Thread.sleep(2000);
 		driver.findElement(By.id("username")).sendKeys("admin");
+		Thread.sleep(2000);
 		driver.findElement(By.name("pwd")).sendKeys("manager");
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("//div[text()='Login ']")).click();
+		Thread.sleep(2000);
 		
 		
 		Assert.assertEquals(driver.getTitle(), "actiTIME - Enter Time-Track");
